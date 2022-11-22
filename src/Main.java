2ce77209475dfcaf04e02da87d7548e072729190
Vlad M <vlad.mackutchev@gmail.com>
2022-11-22 17:32:17 +0600

    add class Proteinbar

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,18 @@ public class Main {
         //chokobar.setPrice(15.99);
         //System.out.println(chokobar);
 
-        VendingMachine vm1 = new VendingMachine("cocka-cola", 3.99);
+        VendingMachine vm1 = new VendingMachine();
+        System.out.println(vm1);
 
         List<Product> prods = new ArrayList<>();
         prods.add(new Product("pie", 2.99));
         prods.add(new Product("ramen", 6.99));
         prods.add(new Product("water", 0.99));
         prods.add(new Milk("fatless milk", 3.49, 0.0));
+        prods.add(new Proteinbar("chocolate bar", 2.99, 3.5, 15.0, 3.0));
         VendingMachine vm2 = new VendingMachine(prods);
         System.out.println(vm2);
-        System.out.println(vm2.getProductBy("pie"));
-        System.out.println(vm2.getProductBy(0.99));
+        //System.out.println(vm2.getProductBy("pie"));
+        //System.out.println(vm2.getProductBy(0.99));
     }
 }
